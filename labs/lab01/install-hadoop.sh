0065c6d37b9fb5123bbb974a1e39f0b6c5212d3a
Artem <datamove@gmail.com>
2018-09-21 11:34:54 +0300

    updated ambari version

--- labs/lab01/install-hadoop.sh
+++ labs/lab01/install-hadoop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # should be run as printf "Y\nn\n1\ny\nn" | ./install-hadoop.sh
-wget -nv http://public-repo-1.hortonworks.com/ambari/ubuntu16/2.x/updates/2.6.2.0/ambari.list -O /etc/apt/sources.list.d/ambari.list
+wget -nv wget http://public-repo-1.hortonworks.com/ambari/ubuntu16/2.x/updates/2.7.0.0/ambari.list -O /etc/apt/sources.list.d/ambari.list
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com B9733A7A07513CAD
 apt-get update
 apt-get -y install ambari-server
-printf "n\n1\ny\nn" | ambari-server setup
+#printf "n\n1\ny\nn" | ambari-server setup
 ambari-server start
